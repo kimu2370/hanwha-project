31ce2f4dc54dfa4b1ff0a54865729972605bba04
kimu2370 <kimu2370@naver.com>
2021-02-25 00:10:27 +0900

    router 수정

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,16 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+    res.send("respond with a resource");
 });
 // req: 클라이언트 정보를 담고 있다.
 // res: 클라이언트 응답을 위한 객체
-router.post('/users', function (req, res, next) {
-    return res.json({
-        name: req.body.name,
-        quote: req.body.quote,
+router.post("/users", function (req, res, next) {
+    res.status(200).json({
+        payload: {
+            name: req.body.name + "server",
+            quote: req.body.quote + "server",
+        },
     });
 });
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,43 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from "react";
 
 const App = () => {
-    const [name, setName] = useState('');
-    const [quote, setQuote] = useState('');
+    const [name, setName] = useState("");
+    const [quote, setQuote] = useState("");
 
     const handleChange = e => {
-        e.target.name === 'name' && setName(e.target.value);
-        e.target.name === 'quote' && setQuote(e.target.value);
+        e.target.name === "name" && setName(e.target.value);
+        e.target.name === "quote" && setQuote(e.target.value);
     };
 
     const handleClick = () => {
-        console.log('clicked');
-
-        fetch('http://localhost:3001/users', {
-            method: 'POST',
+        fetch("http://localhost:3001/users", {
+            method: "POST",
+            body: JSON.stringify({
+                name,
+                quote,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
         }).then(res => {
-            console.log(res);
+            console.log("server response>>>", JSON.stringify(res));
         });
     };
+
+    useEffect(() => {
+        console.log("component mounted");
+        fetch("http://localhost:3001", {
+            method: "GET",
+        }).then(res => {
+            console.log("get response", res);
+        });
+    }, []);
 
     return (
         <>
             <h1>연습 인풋</h1>
-            <input
-                type="text"
-                placeholder="name"
-                name="name"
-                value={name}
-                onChange={handleChange}
-            />
-            <input
-                type="text"
-                placeholder="quote"
-                name="quote"
-                value={quote}
-                onChange={handleChange}
-            />
+            <input type="text" placeholder="name" name="name" value={name} onChange={handleChange} />
+            <input type="text" placeholder="quote" name="quote" value={quote} onChange={handleChange} />
             <button type="submit" onClick={handleClick}>
                 Submit
             </button>
